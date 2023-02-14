95babb0776219b9a9382442b59cafdadb9bc50a8
Ashley Robbins <robbinsa1@central.edu>
2023-02-13 19:21:03 -0600

    updated navigation to use extracted navbar component. laid out resume container with appropriate columns and css breakpoints.

--- web-portfolio/src/app/resume/resume.component.ts
+++ web-portfolio/src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-resume',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent {
-
+  constructor() {
+    NavbarComponent;
+  }
 }
