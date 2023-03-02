525e07be2cc354bb7ed910f89006db4b6c8e8120
a-a-robbins <robbinsa1@central.edu>
2023-03-02 17:59:54 -0600

    Creative collection: 1st iteration (#8)
    * Update README.md
    
    * added components for subpages, laid out creative collection with routing and links, fixed navbar links

---
+++ web-portfolio/src/app/creative-collection/musings/musings.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MusingsComponent } from './musings.component';
+
+describe('MusingsComponent', () => {
+  let component: MusingsComponent;
+  let fixture: ComponentFixture<MusingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MusingsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MusingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
