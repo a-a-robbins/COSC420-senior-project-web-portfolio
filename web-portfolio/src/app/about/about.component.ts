67ed84b4ba7aa8e84f6f81ac2824b31e359c85ce
a-a-robbins <robbinsa1@central.edu>
2023-04-14 12:30:38 -0500

    very messy wip: sidespur code to try and implement a click handler

--- web-portfolio/src/app/about/about.component.ts
+++ web-portfolio/src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {useRef} from "react";
 
 @Component({
@@ -6,47 +6,22 @@ import {useRef} from "react";
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {}
 
-// const canvas = document.getElementById("canvas");
-// const context = canvas.getContext("2d");
-// const width = canvas.width = 320;
-// const height = canvas.height = 480;
-//
-// canvas.style.marginTop = window.innerHeight / 2 - height / 2 + "px";
+export class AboutComponent {
+  ani() {
+    var element = document.getElementById('img');
+    if (element !== null) {
+      element.className = 'classname';
+    }
+  }
 
-// other video
-//var canvas = useRef(document.querySelector('canvas'));
-// var canvas = useRef<HTMLCanvasElement>(document.querySelector('canvas'));
-// console.log("canvas = " + canvas);
-// if(canvas.current !== null) {
-//   var context = useRef<CanvasRenderingContext2D>(canvas.current.getContext('2d'));
-//   console.log("context = " + context);
-//
-//
-//     canvas.current.width = window.innerWidth;
-//     canvas.current.height = window.innerHeight;
-//
-//     var images: any[] = [];
-//     images.length = 8;
-//
-//     for(var i = 0; i < images.length; i++) {
-//       images[i] = new Image();
-//       images[i].src = "../../assets/images/sprites/walk(" + i.toString() + ").png";
-//     }
-//
-//     var j = 1;
-//     setInterval(function() {
-//       j++;
-//       if( j >=8 ) {
-//         j = 1;
-//       }
-//       if(context.current !== null) {
-//         context.current.drawImage(images[i], 100, 100, 100, 100);
-//       }
-//     }, 100)
-//
-// }
+  constructor() {}
+}
+
+
+
+
+
 
 
 
