5c6bc6a744f388c4048c6bc5fbea4f9cb72c417f
a-a-robbins <robbinsa1@central.edu>
2023-04-19 14:07:53 -0500

    move navbar to top-level and move routes to routing-module

--- web-portfolio/src/app/app-routing.module.ts
+++ web-portfolio/src/app/app-routing.module.ts
@@ -1,13 +1,80 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AniTestComponent} from "./ani-test/ani-test.component";
+import {LandingComponent} from "./landing/landing.component";
+import {ResumeComponent} from "./resume/resume.component";
+import {CodeProjectsComponent} from "./code-projects/code-projects.component";
+import {CreativeCollectionComponent} from "./creative-collection/creative-collection.component";
+import {PhotojournalComponent} from "./creative-collection/photojournal/photojournal.component";
+import {MusingsComponent} from "./creative-collection/musings/musings.component";
+import {VisualArtComponent} from "./creative-collection/visual-art/visual-art.component";
+import {AboutComponent} from "./about/about.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'landing', component: LandingComponent},
+  {path: 'resume', component: ResumeComponent},
+  {path: 'code-projects', component: CodeProjectsComponent},
+  {path: 'creative-collection', component: CreativeCollectionComponent},
+  {path: 'creative-collection/photojournal', component: PhotojournalComponent},
+  {path: 'creative-collection/musings', component: MusingsComponent},
+  {path: 'creative-collection/visual-art', component: VisualArtComponent},
+  {path: 'about', component: AboutComponent},
   {path: 'ani-test', component: AniTestComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    BrowserAnimationsModule,
+    RouterModule],
+
+  declarations: [
+    AboutComponent,
+    AniTestComponent,
+    CodeProjectsComponent,
+    CreativeCollectionComponent,
+    MusingsComponent,
+    PhotojournalComponent,
+    VisualArtComponent,
+    LandingComponent,
+    ResumeComponent,
+    NavbarComponent
+  ],
+  exports: [RouterModule, NavbarComponent]
 })
 export class AppRoutingModule { }
--- web-portfolio/src/app/app.module.ts
+++ web-portfolio/src/app/app.module.ts
@@ -3,79 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LandingComponent } from './landing/landing.component';
 import {RouterModule} from "@angular/router";
-import { ResumeComponent } from './resume/resume.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CodeProjectsComponent } from './code-projects/code-projects.component';
-import { CreativeCollectionComponent } from './creative-collection/creative-collection.component';
-import { PhotojournalComponent } from './creative-collection/photojournal/photojournal.component';
-import { MusingsComponent } from './creative-collection/musings/musings.component';
-import { VisualArtComponent } from './creative-collection/visual-art/visual-art.component';
-import { AboutComponent } from './about/about.component';
-import { AniTestComponent } from './ani-test/ani-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingComponent,
-    ResumeComponent,
-    NavbarComponent,
-    CodeProjectsComponent,
-    CreativeCollectionComponent,
-    PhotojournalComponent,
-    MusingsComponent,
-    VisualArtComponent,
-    AboutComponent,
-    AniTestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MdbAccordionModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
-    MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
     BrowserAnimationsModule,
-    RouterModule,
-    RouterModule.forRoot([
-      {path: '', component: LandingComponent},
-      {path: 'landing', component: LandingComponent},
-      {path: 'resume', component: ResumeComponent},
-      {path: 'code-projects', component: CodeProjectsComponent},
-      {path: 'creative-collection', component: CreativeCollectionComponent},
-      {path: 'creative-collection/photojournal', component: PhotojournalComponent},
-      {path: 'creative-collection/musings', component: MusingsComponent},
-      {path: 'creative-collection/visual-art', component: VisualArtComponent},
-      {path: 'about', component: AboutComponent}
-    ])
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
