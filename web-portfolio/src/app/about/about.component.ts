ad176863aeb6a3fcd5a34540d1202c5faae2b028
a-a-robbins <robbinsa1@central.edu>
2023-04-21 15:00:39 -0500

    animations working + added fun css drawings (#16)
    * switched over to JS animations
    
    * got it scrolling again :) also switched to %
    
    * added "adventure paths" and star start point

--- web-portfolio/src/app/about/about.component.ts
+++ web-portfolio/src/app/about/about.component.ts
@@ -8,27 +8,91 @@ import {useRef} from "react";
 })
 
 export class AboutComponent {
-  ani() {
-    var element = document.getElementById('img');
-    if (element !== null) {
-      element.className = 'classname';
-    }
+
+  bezierCurve() {
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    ctx.moveTo(33.76049625, 56.51376); // start point
+    ctx.quadraticCurveTo(
+      117.53628, 247.02303,  // control point 1
+      742.75229, 221.65138   // end point
+    );
   }
 
   palmAnimation() {
-    var element = document.getElementById('sprite');
-    if (element !== null) {
-      element.className = 'sprite-image';
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 50);
+    function frame() {
+        if(posTop != 75 && posLeft != 2) {
+          posTop++;
+          posLeft--;
+          elem.style.top = posTop + "%";
+          elem.style.left = posLeft + "%";
+        } else if(posLeft == 2 && posTop != 75) {
+          posTop++;
+          elem.style.top = posTop + "%";
+        } else if(posTop == 75 && posLeft != 2) {
+          posLeft--;
+          elem.style.left = posLeft + "%";
+        }else {
+          clearInterval(id);
+        }
+      }
+    }
+
+  mountainAnimation() {
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 50);
+    function frame() {
+      if(posTop != 0 && posLeft != 7) {
+        posTop--;
+        posLeft--;
+        elem.style.top = posTop + "%";
+        elem.style.left = posLeft + "%";
+      } else if(posLeft == 7 && posTop != 0) {
+        posTop--;
+        elem.style.top = posTop + "%";
+      } else if(posTop == 0 && posLeft != 7) {
+        posLeft--;
+        elem.style.left = posLeft + "%";
+      }else {
+        clearInterval(id);
+      }
     }
   }
 
   cornAnimation() {
-    var element = document.getElementById('sprite');
-    if (element !== null) {
-      element.className = 'sprite-image'
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 50);
+    function frame() {
+      if(posTop != 35 && posLeft != 82) {
+        posTop--;
+        posLeft++;
+        elem.style.top = posTop + "%";
+        elem.style.left = posLeft + "%";
+      } else if(posLeft == 82 && posTop != 35) {
+        posTop--;
+        elem.style.top = posTop + "%";
+      } else if(posTop == 35 && posLeft != 82) {
+        posLeft++;
+        elem.style.left = posLeft + "%";
+      }else {
+        clearInterval(id);
+      }
     }
   }
-
   constructor() {}
 }
 
--- web-portfolio/src/app/ani-test/ani-test.component.ts
+++ web-portfolio/src/app/ani-test/ani-test.component.ts
@@ -7,8 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AniTestComponent {
   ani() {
-    // @ts-ignore
     document.getElementById('img').className = 'grow';
+  }
+
+  myMove1() {
+    let id: any = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
+      }
+    }
+  }
+
+  myMove2() {
+    let id: any = null;
+    const elem = document.getElementById("animate");
+    let pos = 175;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 200) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
+      }
+    }
   }
 }
 
