19496b6b7d11db42f4ca712a01836b827d1a49d7
a-a-robbins <robbinsa1@central.edu>
2023-04-21 13:06:02 -0500

    got it scrolling again :) also switched to %

--- web-portfolio/src/app/about/about.component.ts
+++ web-portfolio/src/app/about/about.component.ts
@@ -8,6 +8,33 @@ import {useRef} from "react";
 })
 
 export class AboutComponent {
+
+  // palmAnimation() {
+  //   let id: any = null;
+  //   const elem = document.getElementById("sprite");
+  //   let pos = document.getElementById("map-background").clientWidth / 2;
+  //   clearInterval(id);
+  //   id = setInterval(frame, 5);
+  //   function frame() {
+  //     if (pos == 200) {
+  //       clearInterval(id);
+  //     } else {
+  //       pos++;
+  //       elem.style.top = pos + "vh";
+  //       elem.style.left = pos + "vw";
+  //     }
+  //   }
+  // }
+  //
+  // mountainAnimation() {
+  //
+  // }
+  //
+  // cornAnimation() {
+  //
+  // }
+
+
   // ani() {
   //   var element = document.getElementById('img');
   //   if (element !== null) {
@@ -18,28 +45,32 @@ export class AboutComponent {
   palmAnimation() {
     let id: any = null;
     const elem = document.getElementById("sprite");
+    // let pos = 50;
     let posTop = 50;
     let posLeft = 50;
     clearInterval(id);
-    id = setInterval(frame, 5);
+    id = setInterval(frame, 20);
     function frame() {
-      while(posTop != 80 && posLeft != 5) {
-        posTop++;
-        elem.style.top = posTop + "vh";
-        posLeft--;
-        elem.style.left = posLeft + "vw";
-      }
-      if (posTop == 80 && posLeft == 5) {
-        complete();
-        return;
-      }
-
-      function complete() {
-        clearInterval(id);
-        id = null;
+        if(posTop != 80 && posLeft != 5) {
+          posTop++;
+          posLeft--;
+          elem.style.top = posTop + "%";
+          elem.style.left = posLeft + "%";
+        } else if(posLeft == 5 && posTop != 80) {
+          posTop++;
+          elem.style.top = posTop + "%";
+        } else if(posTop == 80 && posLeft != 5) {
+          posLeft--;
+          elem.style.left = posLeft + "%";
+        }else {
+          clearInterval(id);
+          return;
+        }
       }
     }
-  }
+
+
+
 
   cornAnimation() {
     let id: any = null;
