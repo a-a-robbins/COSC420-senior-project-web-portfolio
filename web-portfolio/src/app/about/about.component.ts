86a4a4244f455661a2705edf89defe48fa61f5b4
a-a-robbins <robbinsa1@central.edu>
2023-04-21 14:58:35 -0500

    added "adventure paths" and star start point

--- web-portfolio/src/app/about/about.component.ts
+++ web-portfolio/src/app/about/about.component.ts
@@ -9,124 +9,88 @@ import {useRef} from "react";
 
 export class AboutComponent {
 
-  // palmAnimation() {
-  //   let id: any = null;
-  //   const elem = document.getElementById("sprite");
-  //   let pos = document.getElementById("map-background").clientWidth / 2;
-  //   clearInterval(id);
-  //   id = setInterval(frame, 5);
-  //   function frame() {
-  //     if (pos == 200) {
-  //       clearInterval(id);
-  //     } else {
-  //       pos++;
-  //       elem.style.top = pos + "vh";
-  //       elem.style.left = pos + "vw";
-  //     }
-  //   }
-  // }
-  //
-  // mountainAnimation() {
-  //
-  // }
-  //
-  // cornAnimation() {
-  //
-  // }
-
-
-  // ani() {
-  //   var element = document.getElementById('img');
-  //   if (element !== null) {
-  //     element.className = 'classname';
-  //   }
-  // }
+  bezierCurve() {
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    ctx.moveTo(33.76049625, 56.51376); // start point
+    ctx.quadraticCurveTo(
+      117.53628, 247.02303,  // control point 1
+      742.75229, 221.65138   // end point
+    );
+  }
 
   palmAnimation() {
     let id: any = null;
     const elem = document.getElementById("sprite");
-    // let pos = 50;
     let posTop = 50;
     let posLeft = 50;
     clearInterval(id);
-    id = setInterval(frame, 20);
+    id = setInterval(frame, 50);
     function frame() {
-        if(posTop != 80 && posLeft != 5) {
+        if(posTop != 75 && posLeft != 2) {
           posTop++;
           posLeft--;
           elem.style.top = posTop + "%";
           elem.style.left = posLeft + "%";
-        } else if(posLeft == 5 && posTop != 80) {
+        } else if(posLeft == 2 && posTop != 75) {
           posTop++;
           elem.style.top = posTop + "%";
-        } else if(posTop == 80 && posLeft != 5) {
+        } else if(posTop == 75 && posLeft != 2) {
           posLeft--;
           elem.style.left = posLeft + "%";
         }else {
           clearInterval(id);
-          return;
         }
       }
     }
 
-
-
+  mountainAnimation() {
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 50);
+    function frame() {
+      if(posTop != 0 && posLeft != 7) {
+        posTop--;
+        posLeft--;
+        elem.style.top = posTop + "%";
+        elem.style.left = posLeft + "%";
+      } else if(posLeft == 7 && posTop != 0) {
+        posTop--;
+        elem.style.top = posTop + "%";
+      } else if(posTop == 0 && posLeft != 7) {
+        posLeft--;
+        elem.style.left = posLeft + "%";
+      }else {
+        clearInterval(id);
+      }
+    }
+  }
 
   cornAnimation() {
     let id: any = null;
     const elem = document.getElementById("sprite");
-
     let posTop = 50;
     let posLeft = 50;
     clearInterval(id);
-    id = setInterval(frame, 5);
-
+    id = setInterval(frame, 50);
     function frame() {
-      while(posTop != 40 && posLeft != 75) {
+      if(posTop != 35 && posLeft != 82) {
         posTop--;
-        elem.style.top = posTop + "vh";
         posLeft++;
-        elem.style.left = posLeft + "vw";
-      }
-
-      if(posTop == 40 && posLeft == 75) {
-        complete();
-        return;
-      }
-    }
-
-    function complete() {
-      clearInterval(id);
-      id = null;
-    }
-  }
-
-  mountainAnimation() {
-    let id: any = null;
-    const elem = document.getElementById("sprite");
-
-    let posTop = 50;
-    let posLeft = 50;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-
-    function frame() {
-      while(posTop != 5 && posLeft != 10) {
+        elem.style.top = posTop + "%";
+        elem.style.left = posLeft + "%";
+      } else if(posLeft == 82 && posTop != 35) {
         posTop--;
-        elem.style.top = posTop + "vh";
-        posLeft--;
-        elem.style.left = posLeft + "vw";
+        elem.style.top = posTop + "%";
+      } else if(posTop == 35 && posLeft != 82) {
+        posLeft++;
+        elem.style.left = posLeft + "%";
+      }else {
+        clearInterval(id);
       }
-
-      if(posTop == 5 && posLeft == 10) {
-        complete();
-        return;
-      }
-    }
-
-    function complete() {
-      clearInterval(id);
-      id = null;
     }
   }
   constructor() {}
