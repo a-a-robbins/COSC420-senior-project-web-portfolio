9e2184f19958f7674006e639704e9c4bda1b31db
a-a-robbins <robbinsa1@central.edu>
2023-04-20 22:48:32 -0500

    switched over to JS animations

--- web-portfolio/src/app/about/about.component.ts
+++ web-portfolio/src/app/about/about.component.ts
@@ -8,27 +8,96 @@ import {useRef} from "react";
 })
 
 export class AboutComponent {
-  ani() {
-    var element = document.getElementById('img');
-    if (element !== null) {
-      element.className = 'classname';
-    }
-  }
+  // ani() {
+  //   var element = document.getElementById('img');
+  //   if (element !== null) {
+  //     element.className = 'classname';
+  //   }
+  // }
 
   palmAnimation() {
-    var element = document.getElementById('sprite');
-    if (element !== null) {
-      element.className = 'sprite-image';
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      while(posTop != 80 && posLeft != 5) {
+        posTop++;
+        elem.style.top = posTop + "vh";
+        posLeft--;
+        elem.style.left = posLeft + "vw";
+      }
+      if (posTop == 80 && posLeft == 5) {
+        complete();
+        return;
+      }
+
+      function complete() {
+        clearInterval(id);
+        id = null;
+      }
     }
   }
 
   cornAnimation() {
-    var element = document.getElementById('sprite');
-    if (element !== null) {
-      element.className = 'sprite-image'
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+
+    function frame() {
+      while(posTop != 40 && posLeft != 75) {
+        posTop--;
+        elem.style.top = posTop + "vh";
+        posLeft++;
+        elem.style.left = posLeft + "vw";
+      }
+
+      if(posTop == 40 && posLeft == 75) {
+        complete();
+        return;
+      }
+    }
+
+    function complete() {
+      clearInterval(id);
+      id = null;
     }
   }
 
+  mountainAnimation() {
+    let id: any = null;
+    const elem = document.getElementById("sprite");
+
+    let posTop = 50;
+    let posLeft = 50;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+
+    function frame() {
+      while(posTop != 5 && posLeft != 10) {
+        posTop--;
+        elem.style.top = posTop + "vh";
+        posLeft--;
+        elem.style.left = posLeft + "vw";
+      }
+
+      if(posTop == 5 && posLeft == 10) {
+        complete();
+        return;
+      }
+    }
+
+    function complete() {
+      clearInterval(id);
+      id = null;
+    }
+  }
   constructor() {}
 }
 
