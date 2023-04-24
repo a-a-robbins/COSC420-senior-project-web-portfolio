117d8bd7650df8985960eb14824a0b673ff7b8a9
a-a-robbins <robbinsa1@central.edu>
2023-04-24 09:57:52 -0500

    downloader successful!!

---
+++ web-portfolio/src/app/downloader/downloader.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-downloader',
+  templateUrl: './downloader.component.html',
+  styleUrls: ['./downloader.component.scss']
+})
+export class DownloaderComponent {
+
+}
