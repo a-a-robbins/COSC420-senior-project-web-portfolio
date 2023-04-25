009baceca84243a67fe2ffc8ade659cdfe1ca32a
a-a-robbins <robbinsa1@central.edu>
2023-04-25 08:06:14 -0500

    created hidden divs and buttons for photojournal

--- web-portfolio/src/app/creative-collection/photojournal/photojournal.component.ts
+++ web-portfolio/src/app/creative-collection/photojournal/photojournal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { ModalComponent } from "../../modal/modal.component";
 
 @Component({
   selector: 'app-photojournal',
@@ -6,5 +8,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./photojournal.component.scss']
 })
 export class PhotojournalComponent {
+
+  badlandsInfo() {
+    const elem = document.getElementById("badlands-more");
+    const moab = document.getElementById("moab-more");
+    const sanDiego = document.getElementById("san-diego-more");
+    const sequim = document.getElementById("sequim-more");
+
+    if(moab && moab.style.display == "block") {
+      moab.style.display = "none";
+    }
+
+    if(sequim && sequim.style.display == "block") {
+      sequim.style.display = "none";
+    }
+
+    if(sanDiego && sanDiego.style.display == "block") {
+      sanDiego.style.display = "none";
+    }
+
+    if (elem || elem.style.display == "none") {
+      elem.style.display = "block";
+    }
+  }
+
+  moabInfo() {
+    const elem = document.getElementById("moab-more");
+    const badlands = document.getElementById("badlands-more");
+    const sanDiego = document.getElementById("san-diego-more");
+    const sequim = document.getElementById("sequim-more");
+
+    if(badlands && badlands.style.display == "block") {
+      badlands.style.display = "none";
+    }
+
+    if(sequim && sequim.style.display == "block") {
+      sequim.style.display = "none";
+    }
+
+    if(sanDiego && sanDiego.style.display == "block") {
+      sanDiego.style.display = "none";
+    }
+
+    if (elem || elem.style.display == "none") {
+      elem.style.display = "block";
+    }
+  }
+  sanDiegoInfo() {
+    const elem = document.getElementById("san-diego-more");
+    const badlands = document.getElementById("badlands-more");
+    const moab = document.getElementById("moab-diego-more");
+    const sequim = document.getElementById("sequim-more");
+
+    if(badlands && badlands.style.display == "block") {
+      badlands.style.display = "none";
+    }
+
+    if(moab && moab.style.display == "block") {
+      moab.style.display = "none";
+    }
+
+    if(sequim && sequim.style.display == "block") {
+      sequim.style.display = "none";
+    }
+
+    if (elem || elem.style.display == "none") {
+      elem.style.display = "block";
+    }
+  }
+
+  sequimInfo() {
+    const elem = document.getElementById("sequim-more");
+    const badlands = document.getElementById("badlands-more");
+    const moab = document.getElementById("moab-diego-more");
+    const sanDiego = document.getElementById("san-diego-more");
+
+    if(badlands && badlands.style.display == "block") {
+      badlands.style.display = "none";
+    }
+
+    if(moab && moab.style.display == "block") {
+      moab.style.display = "none";
+    }
+
+    if(sanDiego && sanDiego.style.display == "block") {
+      sanDiego.style.display = "none";
+    }
+
+    if (elem || elem.style.display == "none") {
+      elem.style.display = "block";
+    }
+  }
 
 }
