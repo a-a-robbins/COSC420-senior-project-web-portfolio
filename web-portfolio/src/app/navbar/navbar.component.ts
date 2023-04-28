9a7192f3e484115d05d774ba3966c82fe0264262
Joseph R Gravlin <joeygravlin@gmail.com>
2023-04-27 22:39:23 -0400

    Refactor `NavbarComponent` links as `NavigationItem[]` data and template`

--- web-portfolio/src/app/navbar/navbar.component.ts
+++ web-portfolio/src/app/navbar/navbar.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+type NavigationItem = {
+  routerLink: string,
+  name: string,
+  children?: NavigationItem[],
+  id?: string
+};
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,4 +14,19 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  readonly leftNavigationItems: NavigationItem[] = [
+    { routerLink: '/about', name: 'About' },
+    { routerLink: '/code-projects', name: 'Code Projects' },
+    {
+      routerLink: '/creative-collection',
+      name: 'Creative Collection',
+      children: [
+        { routerLink: 'photojournal', name: 'Photojournal' },
+        { routerLink: 'musings', name: 'Musings' },
+        { routerLink: 'visual-art', name: 'Visual Art' },
+      ],
+      id: 'navbarDropdownMenuLink',
+    },
+    { routerLink: '/resume', name: 'Resume' },
+  ];
 }
