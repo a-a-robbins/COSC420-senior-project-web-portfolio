d4e3d7320e800a61bd0971b605cac0bd40cb1786
a-a-robbins <a.a.robbing@gmail.com>
2025-09-02 19:00:42 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# COSC420-senior-project-web-portfolio
-An Angular-based web portfolio written in TypeScript, C# and styled with Bootstrap
--- web-portfolio/src/app/downloader/downloader.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-downloader',
-  templateUrl: './downloader.component.html',
-  styleUrls: ['./downloader.component.scss']
-})
-export class DownloaderComponent {
-
-}
